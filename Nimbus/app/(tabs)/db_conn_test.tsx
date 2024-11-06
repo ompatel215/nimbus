714bfda8a382228ae26d5d0e1040392a47041157
Nate Wagenhoffer <77755517+wagenhoffer@users.noreply.github.com>
2024-11-06 19:18:20 +0000

    auth failed, rest might be good

--- Nimbus/app/(tabs)/db_conn_test.tsx
+++ Nimbus/app/(tabs)/db_conn_test.tsx
@@ -1,14 +1,14 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 // import axios from 'axios';
 
 // Replace these with your MongoDB Cloud connection details
 
 const MONGODB_URI = 'mongodb+srv://tml5800:1XEhIJXCusFyVKre@cluster0.jz4kj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const DATABASE_NAME = 'Nimbus';
-const COLLECTION_NAME = 'players';
+// const DATABASE_NAME = 'Nimbus';
+// const COLLECTION_NAME = 'players';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(MONGODB_URI, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
